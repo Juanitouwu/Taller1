810e10ae8c45d798731c5bfd7d60326da63200bd
juanca0108 <109818872+Juanitouwu@users.noreply.github.com>
2022-09-27 12:02:51 -0500

    Algunas vista arregladas

--- js/validacion.js
+++ js/validacion.js
@@ -129,3 +129,20 @@ form.addEventListener('submit',e=>{
         }
 
 })
+let btnmodalv=document.getElementById("terminos");
+let venmodalv=document.getElementById("ventanaModal");
+let cermodalv=document.querySelector(".cerrarModal");
+btnmodalv.addEventListener('click', (e)=>{
+    e.preventDefault();
+    venmodalv.style.display="block"
+})
+
+cermodalv.addEventListener('click',()=>{
+    venmodalv.style.display="none"
+})
+
+window.addEventListener('click',(e)=>{
+    if(e.target==venmodalv){
+        venmodalv.style.display="none"
+    }
+})
