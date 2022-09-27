9768dae17f0dd279cc7a46b640fb39dd6c7ef982
juanca0108 <109818872+Juanitouwu@users.noreply.github.com>
2022-09-27 08:08:47 -0500

    Delete Taller1-Inicio directory

--- Taller1-Inicio/js/calculadora.js
+++
@@ -1,9 +0,0 @@
-function calcular(){
-    let num1=parseInt(document.getElementById("num1".value));
-    let num2=parseInt(document.getElementById("num2".value));
-    let op=document.getElementById("operacion").value;
-    if(op==1){
-        let res=num1+num2;
-        alert(`El numero 1 es ${num1} y el numero ${num2} la suma es ${res}`);
-    }
-}
